0cfdc5893c31d919811714603a5b1e1d61ffdce0
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-27 23:49:47 +0300

    installed nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('111 Hello, World!');
 })
 
 app.listen(4444, (err) => {
