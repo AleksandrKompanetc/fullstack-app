a32e70f90e6d6d8c25af0d18c72a9be438978066
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-31 23:15:44 +0300

    changed web server

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('Server OK')
+})
