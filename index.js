fc001e44881a23499ec709edf431b4cf798c567d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-31 23:45:13 +0300

    added post request

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('111 Hello, World!');
 });
 
+app.post('auth/login', (req, res) => {
+  res.json({
+    success: true
+  })
+})
+
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err)
