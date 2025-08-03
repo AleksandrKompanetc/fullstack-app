228287c29deb7e0b6828a6f76e95b35fc6a2b23b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-03 23:19:04 +0300

    using express json

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.post('auth/login', (req, res) => {
+app.post('/auth/login', (req, res) => {
   res.json({
     success: true,
-  })
-})
+  });
+});
 
 app.listen(4444, (err) => {
   if (err) {
