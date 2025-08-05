bc3a0dbc44836620ff666a5889c1ee8c08e84ab3
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-05 23:46:16 +0300

    changed index.js

--- index.js
+++ index.js
@@ -13,23 +13,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
 
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign({
-    email: req.body.email,
-    fullname: 'John'
-  },
-  'secret123',
-)
-  res.json({
-    success: true,
-    token
-  });
+app.post('/auth/register', (req, res) => {
+    
 });
 
 app.listen(4444, (err) => {
