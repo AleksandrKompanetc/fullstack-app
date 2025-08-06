9dd9db03e5cfb8e64187070d17d7079731acd310
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-06 23:46:47 +0300

    creating user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 import {validationResult} from 'express-validator'
 
@@ -22,18 +23,24 @@ app.use(express.json());
 //   res.send('Hello, World!');
 // });
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
   const doc = new UserModel({
     email: req.body.email,
     fullname: req.body.fullname,
     avatarUrl: req.body.avatarUrl,
-    passwordHash: 
+    passwordHash,
   })
+
+  const user = await doc.save();
 
   res.json({
     success: true,
