01be93ee51320899ca4f49146050e3d1be06c226
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-08 23:28:09 +0300

    added password hash

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
-import {validationResult} from 'express-validator'
+import { validationResult } from 'express-validator'
 
-import {registerValidation} from './validations/auth.js'
+import { registerValidation } from './validations/auth.js'
 
 import UserModel from './models/User.js'
 
 mongoose.connect()
-.then(() => {
-  console.log('MongoDB connected')
-}).catch((err) => {
-  console.error('MongoDB connection error:', err)
-})
+  .then(() => {
+    console.log('MongoDB connected')
+  }).catch((err) => {
+    console.error('MongoDB connection error:', err)
+  })
 
 const app = express();
 
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.post('/auth/login', async (req, res) => {
   try {
-    const user = await UserModel.findOne({email: req.body.email});
+    const user = await UserModel.findOne({ email: req.body.email });
 
     if (!user) {
       return res.status(404).json({
@@ -34,7 +34,7 @@ app.post('/auth/login', async (req, res) => {
     }
 
     const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-    
+
     if (!isValidPassword) {
       return res.status(404).json({
         message: 'Invalid login or password',
@@ -51,44 +51,50 @@ app.post('/auth/login', async (req, res) => {
       },
     )
 
-  } catch (err) {}
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) { }
 })
 
 app.post('/auth/register', registerValidation, async (req, res) => {
   try {
     const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-  const doc = new UserModel({
-    email: req.body.email,
-    fullname: req.body.fullname,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: hash,
-  })
+    const doc = new UserModel({
+      email: req.body.email,
+      fullname: req.body.fullname,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    })
 
-  const user = await doc.save();
+    const user = await doc.save();
 
-  const token = jwt.sign({
-    _id: user._id,
-  },
-  'secret123',
-  {
-    expiresIn: '30d',
-  }
-)
+    const token = jwt.sign({
+      _id: user._id,
+    },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    )
 
-  const {passwordHash, ...userData} = user._doc;
+    const { passwordHash, ...userData } = user._doc;
 
-  res.json({
-    ...userData,
-    token,
-  });
+    res.json({
+      ...userData,
+      token,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({
