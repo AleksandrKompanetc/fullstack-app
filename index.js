164c4a20eee5e946d37cd15c755d34456421a654
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-08 23:03:12 +0300

    added hash

--- index.js
+++ index.js
@@ -32,16 +32,17 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
   const password = req.body.password;
   const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
+  const hash = await bcrypt.hash(password, salt);
 
   const doc = new UserModel({
     email: req.body.email,
     fullname: req.body.fullname,
     avatarUrl: req.body.avatarUrl,
-    passwordHash,
+    passwordHash: hash,
   })
 
   const user = await doc.save();
+
   const token = jwt.sign({
     _id: user._id,
   },
@@ -51,8 +52,10 @@ app.post('/auth/register', registerValidation, async (req, res) => {
   }
 )
 
+  const {passwordHash, ...userData} = user._doc;
+
   res.json({
-    ...user._doc,
+    ...userData,
     token,
   });
   } catch (err) {
