c802af77e3325bca13aa4a1f1d8c33a2aa689c10
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-12 22:34:04 +0300

    set header

--- app_02.js
+++ app_02.js
@@ -4,5 +4,6 @@ http.createServer(function(req, res) {
   console.log(req.url);
   console.log(req.method);
   console.log('server work');
-  res.end('1');
-}).listen(3600);
+  res.setHeader('Content-Type', 'text/html; charset=utf-8;');
+  res.end('<h2>hello world</h2>');
+}).listen(3700);
