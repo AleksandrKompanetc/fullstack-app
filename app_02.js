0cdefd7d69410d3a6822b8b37482ff709ae7e7e1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-12 22:38:14 +0300

    res write

--- app_02.js
+++ app_02.js
@@ -5,5 +5,7 @@ http.createServer(function(req, res) {
   console.log(req.method);
   console.log('server work');
   res.setHeader('Content-Type', 'text/html; charset=utf-8;');
-  res.end('<h2>hello world</h2>');
+  res.write('<h2>hello world</h2>');
+  res.write('<p>this is a test</p>');
+  res.end();
 }).listen(3700);
