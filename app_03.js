c9938184734c11db1e5a2da1d44b519eb83bb98a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-12 23:10:02 +0300

    changed switch

--- app_03.js
+++ app_03.js
@@ -10,7 +10,14 @@ http.createServer(function(req, res) {
       console.log('main page');
       res.write('<h1>Main page</h1>');
       break;
+    case '/contact':
+      console.log('Contact page');
+      res.write('<h1>Contact</h1>');
+      break;
+    default:
+      console.log('404');
+      res.write('<h1>404</h1>');
   }
   res.end();
-  
+
 }).listen(PORT);
