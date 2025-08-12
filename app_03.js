afa2eaa7d09d3469d12b0005664fc6be934820cf
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-12 23:06:16 +0300

    added switch for url in server

--- app_03.js
+++ app_03.js
@@ -1,3 +1,16 @@
+const http = require('http');
+const PORT = 3501;
+
 http.createServer(function(req, res) {
+  const url = req.url;
+  console.log(url);
+
+  switch (url) {
+    case '/':
+      console.log('main page');
+      res.write('<h1>Main page</h1>');
+      break;
+  }
+  res.end();
   
-})
+}).listen(PORT);
