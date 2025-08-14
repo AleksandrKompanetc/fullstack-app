ccf365d012a28cca5c9956613ca2db544d9e931a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-14 16:35:00 +0300

    added image

---
+++ app_05/app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = 3506;
+
+http.createServer(function (req, res) {
+  const url = req.url;
+  console.log(url);
+
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+  switch (url) {
+    case '/':
+      console.log('main page');
+      res.write('<h1>Main Page</h1>');
+      break;
+    case '/contact':
+      console.log('Contact');
+      let data = fs.readFileSync('./public/contact.html', { encoding: 'utf8', flag: 'r' });
+      res.write(data);
+      break;
+    default:
+      if (url.includes('/images')) {
+        console.log('Image =>>>>>');
+        let data = fs.readFileSync('./public' + url);
+        res.write(data);
+      } else {
+        console.log('404');
+        res.write('<h1>404</h1>');
+      }
+  }
+  res.end();
+
+}).listen(PORT);
