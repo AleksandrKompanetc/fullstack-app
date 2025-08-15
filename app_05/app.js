3d72a484f24613362f22ee076e7d2232a6ba20e7
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-15 22:50:26 +0300

    adding res end

--- app_05/app.js
+++ app_05/app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const PORT = 3507;
+const PORT = 3508;
 
 http.createServer(function (req, res) {
   const url = req.url;
@@ -12,25 +12,29 @@ http.createServer(function (req, res) {
     case '/':
       console.log('main page');
       res.write('<h1>Main Page</h1>');
+      res.end();
       break;
     case '/contact':
       console.log('Contact');
       let data = fs.readFileSync('./public/contact.html', { encoding: 'utf8', flag: 'r' });
       res.write(data);
+      res.end();
       break;
     default:
       if (url.includes('/images')) {
         console.log('Image =>>>>>');
-        fs.readFile('./public' + url, {}, function (error, data) {
+        fs.readFile('./public'+ url, {}, function (error, data) {
           if (error) {
             console.log('======= get =======');
             res.setHeader('Content-Type', 'image/png');
             res.write(data);
+            res.end();
           }
         });
       } else {
         console.log('404');
         res.write('<h1>404</h1>');
+        res.end();
       }
   }
   res.end();
