749498039b5d143b8bf4c763e7144c07e74d5c83
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-15 23:58:57 +0300

    changed function staticFile

--- app_06/app.js
+++ app_06/app.js
@@ -32,7 +32,7 @@ const mimeTypes = {
 
 function staticFile(res, filePath, ext) {
   res.setHeader('Content-Type', mimeTypes[ext]);
-  fs.readFile('./public'+filePath, {encoding: 'utf8', flag: 'r'}, (error, data) => {
+  fs.readFile('./public'+filePath, (error, data) => {
     if (error) {
       res.statusCode = 404;
       res.end();
